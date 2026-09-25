6224317eea32311f015732d84daa577b387717c9
Chuye Princely Tata <princelychuye@gmail.com>
2026-09-25 16:30:51 +0100

    Wrap LoginView in Suspense to fix prerender build error

--- client/src/app/(auth)/login/page.jsx
+++ client/src/app/(auth)/login/page.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { LoginView } from "./LoginView";
 
 export const metadata = {
@@ -6,5 +7,9 @@ export const metadata = {
 };
 
 export default function LoginPage() {
-  return <LoginView />;
+  return (
+    <Suspense>
+      <LoginView />
+    </Suspense>
+  );
 }
